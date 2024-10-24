c17e1e09aaada021897e09af331dbdde2ea4450a
rolyPolyVole <yezhanting@gmail.com>
2024-10-25 00:36:53 +0100

    Move game housekeeping events from main class to here

--- src/main/java/foundation/esoteric/minecraft/plugins/games/fireworkwars/events/game/HouseKeepingListener.java
+++ src/main/java/foundation/esoteric/minecraft/plugins/games/fireworkwars/events/game/HouseKeepingListener.java
@@ -4,12 +4,17 @@ import com.destroystokyo.paper.event.player.PlayerSetSpawnEvent;
 import foundation.esoteric.minecraft.plugins.games.fireworkwars.FireworkWarsPlugin;
 import foundation.esoteric.minecraft.plugins.games.fireworkwars.game.FireworkWarsGame;
 import foundation.esoteric.minecraft.plugins.games.fireworkwars.game.team.TeamPlayer;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.CauldronLevelChangeEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class HouseKeepingListener implements Listener {
     private final FireworkWarsPlugin plugin;
@@ -26,6 +31,28 @@ public class HouseKeepingListener implements Listener {
 
     public void unregister() {
         HandlerList.unregisterAll(this);
+    }
+
+    @EventHandler
+    public void onDamage(@NotNull EntityDamageEvent event) {
+        World world = event.getEntity().getWorld();
+
+        if (event.getEntity() instanceof Villager) {
+            return;
+        }
+
+        if (!game.isPlaying() && game.usesWorld(world.getName())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(@NotNull BlockBreakEvent event) {
+        World world = event.getBlock().getWorld();
+
+        if (!game.isPlaying() && game.usesWorld(world.getName())) {
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler
